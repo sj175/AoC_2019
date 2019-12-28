d030c6a5a4ead0fe83bcb5c607a8ef585e8b4e57
Sam Jones <sam.jones@splend.com>
2019-12-28 10:45:52 +0000

    add skeleton for part 2

--- day4.py
+++ day4.py
@@ -27,8 +27,30 @@ def check_number(problem_input: str) -> bool:
     return double
 
 
+def check_number_2(problem_input: str) -> bool:
+    # two adjacent digits are the same
+    previous = 0
+    double = False
+    for current in range(1, len(problem_input)):
+        if int(problem_input[current]) < int(problem_input[previous]):
+            return False
+        if int(problem_input[previous]) == int(problem_input[current]):
+            double = True
+        previous += 1
+
+    return double
+
+
 def part_2(problem_input: str) -> int:
-    pass
+    start = 382345
+    end = 843167
+    total = 0
+
+    for number in range(start, end + 1):
+        if check_number_2(str(number)):
+            total += 1
+
+    return total
 
 
 def main():
@@ -39,7 +61,16 @@ def main():
     x = "123789"
     print(check_number(x) is False)
 
-    print(part_1())
+    print("PART 1:", part_1())
+
+    x = "112233"
+    print(check_number_2(x) is True)
+    x = "123444"
+    print(check_number_2(x) is False)
+    x = "111122"
+    print(check_number_2(x) is False)
+
+    print("PART 2:")
 
 
 if __name__ == '__main__':
