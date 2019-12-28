cfa97acf35fdf2cc0247c7e0025dd98d4fc0d7e6
Sam Jones <sam.jones@splend.com>
2019-12-28 22:17:56 +0000

    add day6 input

---
+++ day5.py
@@ -0,0 +1,15 @@
+def part_1() -> int:
+    return 0
+
+
+def part_2() -> int:
+    return 0
+
+
+def main():
+    x = "111111"
+    print(part_1(x) is True)
+
+
+if __name__ == '__main__':
+    main()
---
+++ day6.py
@@ -0,0 +1,25 @@
+def read_input() -> str:
+    with open('day6.txt', 'r') as f:
+        return f.read()
+
+
+def part_1(problem_input: str):
+    lines = problem_input.split("\n")
+
+
+def main():
+    part_1("""COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L""")
+
+
+if __name__ == '__main__':
+    main()
