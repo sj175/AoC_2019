1b596e84d8c177c2dc39f54c590ab9a43e87a1dd
Sam Jones <sam.jones@splend.com>
2019-12-29 12:43:32 +0000

    part 2 example works

--- day6.py
+++ day6.py
@@ -28,6 +28,26 @@ def find_links(to: str, lines: List[str]) -> List[str]:
     return output
 
 
+def find_orbital_parent(of: str, lines: List[str]) -> str:
+    for line in lines:
+        if line.split(")")[1] == of:
+            return line.split(")")[0]
+
+    return ""
+
+
+def find_links_both_directions(to: str, lines: List[str]) -> List[str]:
+    output = []
+
+    for line in lines:
+        if line.split(")")[0] == to:
+            output.append(line.split(")")[1])
+        if line.split(")")[1] == to:
+            output.append(line.split(")")[0])
+
+    return output
+
+
 def part_1(problem_input: str) -> int:
     lines = problem_input.split("\n")
     root = "COM"
@@ -47,6 +67,25 @@ def part_1(problem_input: str) -> int:
     return total
 
 
+def part_2(problem_input: str, start: str, end: str) -> int:
+    lines = problem_input.split("\n")
+    start = find_orbital_parent(start, lines)
+    end = find_orbital_parent(end, lines)
+
+    to_visit = [(start, 0)]
+    while to_visit:
+        current = to_visit[0]
+        to_visit = to_visit[1:]
+        count = current[1]
+        if current[0] == end:
+            return count
+        links = find_links_both_directions(current[0], lines)
+        for node in links:
+            to_visit.append((node, count + 1))
+
+    return 0
+
+
 def main():
     print(part_1("""COM)B
 B)C
@@ -61,6 +100,22 @@ J)K
 K)L""") == 42)
 
     print(part_1(read_input()))
+
+    print(part_2("""COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+K)YOU
+I)SAN""", "YOU", "SAN"))
+
+    #print(part_2(read_input(), "YOU", "SAN"))
 
 
 if __name__ == '__main__':
