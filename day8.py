ffdf099ce79cb0504007cbe42fc19518b89e27af
Sam Jones <sam.jones@splend.com>
2019-12-30 00:27:47 +0000

    part 2 produces matrix

--- day8.py
+++ day8.py
@@ -34,9 +34,23 @@ def part_1(problem_input: str, width=25, height=6) -> int:
     return layer_count(min_layer, "1") * layer_count(min_layer, "2")
 
 
-def part_2(problem_input: str):
-    pass
+def part_2(problem_input: str, width=25, height=6) -> List[List[int]]:
+    layers = list(grouper(height, list(grouper(width, problem_input))))
+
+    final_layer = [[-1]*25 for _ in range(6)]
+
+    for layer in layers:
+        for y in range(height):
+            for x in range(width):
+                if layer[y][x] != '2':
+                    if final_layer[y][x] == -1:
+                        final_layer[y][x] = layer[y][x]
+
+    return final_layer
 
 
 if __name__ == '__main__':
     print(part_1(read_input(), 25, 6))
+
+    for row in part_2(read_input()):
+        print(row)
