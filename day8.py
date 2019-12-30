1cfd881973b255bfc40bd8775d361b0195cf020f
Sam Jones <sam.jones@splend.com>
2019-12-30 00:54:13 +0000

    day8 part 2 image working

--- day8.py
+++ day8.py
@@ -1,5 +1,7 @@
 from typing import List, Tuple
 from itertools import zip_longest
+import numpy as np
+from PIL import Image
 
 
 def read_input() -> str:
@@ -37,20 +39,35 @@ def part_1(problem_input: str, width=25, height=6) -> int:
 def part_2(problem_input: str, width=25, height=6) -> List[List[int]]:
     layers = list(grouper(height, list(grouper(width, problem_input))))
 
-    final_layer = [[-1]*25 for _ in range(6)]
+    final_layer = [[-1] * 25 for _ in range(6)]
 
     for layer in layers:
         for y in range(height):
             for x in range(width):
                 if layer[y][x] != '2':
                     if final_layer[y][x] == -1:
-                        final_layer[y][x] = layer[y][x]
+                        final_layer[y][x] = int(layer[y][x])
 
     return final_layer
+
+
+def plebbing(inp: List[List[int]]) -> List[List[Tuple[int, int, int]]]:
+    output = [[-1]*25 for _ in range(6)]
+
+    for y in range(6):
+        for x in range(25):
+            if inp[y][x] == 0:
+                output[y][x] = (255, 255, 255)
+            elif inp[y][x] == 1:
+                output[y][x] = (0, 0, 0)
+            else:
+                print("fuck")
+
+    return output
 
 
 if __name__ == '__main__':
     print(part_1(read_input(), 25, 6))
 
-    for row in part_2(read_input()):
-        print(row)
+    img = Image.fromarray(np.asarray(plebbing(part_2(read_input())), dtype=np.uint8))
+    img.save('day8.png')
